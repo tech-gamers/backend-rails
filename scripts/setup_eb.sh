f9caf94ab037be9d9ed439f4648423088803ad8a
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-10 04:44:28 -0700

    bind puma pidfile to EB nginx upstream config

--- scripts/setup_eb.sh
+++ scripts/setup_eb.sh
@@ -2,10 +2,11 @@ eb init
 
 rails credentials:edit
 
-# Skip setting up the RDS so we can confirm EB is working
-# After RDS setup we should turn off RAILS_SKIP_MIGRATION
+# 1. Skip setting up the RDS so we can confirm EB is working
+# 2. https://stackoverflow.com/a/40912354/2214973
 eb setenv \
   RAILS_MASTER_KEY=$(cat "$(dirname $0)/../config/master.key") \
   RAILS_SKIP_ASSET_COMPILATION=true \
-  RAILS_SKIP_MIGRATIONS=true
+  RAILS_SKIP_MIGRATIONS=true \
+  PIDFILE=/var/run/puma/my_app.sock
 
