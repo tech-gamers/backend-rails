530605f480f0d3931d7ba0dc629f98dfadac173b
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-10 01:11:56 -0700

    [T] EB skip DB migration

--- scripts/setup_eb.sh
+++ scripts/setup_eb.sh
@@ -2,7 +2,10 @@ eb init
 
 rails credentials:edit
 
+# Skip setting up the RDS so we can confirm EB is working
+# After RDS setup we should turn off RAILS_SKIP_MIGRATION
 eb setenv \
   RAILS_MASTER_KEY=$(cat "$(dirname $0)/../config/master.key") \
-  RAILS_SKIP_ASSET_COMPILATION=true
+  RAILS_SKIP_ASSET_COMPILATION=true \
+  RAILS_SKIP_MIGRATIONS=true
 
