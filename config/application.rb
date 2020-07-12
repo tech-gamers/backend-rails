6ee3a756199110d7ee43d7bdf6249d41f1467b26
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-11 23:54:59 -0700

    allow host

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module Backend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.hosts << 'liu-backend.com'
 
     # Don't generate system test files.
     config.generators.system_tests = nil
