2b9870c2a792e0049f6fbacb84a5c78bf2e0fb95
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-13 02:21:04 -0700

    revoke schema file

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,4 @@
 ActiveRecord::Schema.define(version: 2020_07_13_072121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
-
-  create_table 'issues', force: :cascade do |t|
-    t.string 'title'
-    t.string 'content'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-  end
 end
