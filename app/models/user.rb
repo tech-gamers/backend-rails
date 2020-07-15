dea9e32e46c51a48c060f48af310714ed333df7f
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-14 21:15:11 -0700

    implement User.from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   devise :lockable, :omniauthable, :rememberable, :trackable
+
+  class << self
+    def from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.name = auth.info.name
+        user.uid = auth.uid
+        user.email = auth.info.email
+      end
+    end
+  end
 end
