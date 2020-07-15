7c29b32df11a2306e76aa05084f0c1d138ea1984
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-15 00:13:14 -0700

    sign in & sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  get 'sign_in', to: 'users/sessions#new', as: :new_session
+  as :user do
+    get 'sign_in', to: 'sessions#new', as: :new_session
+  end
   get 'sign_up', to: 'pages#sign_up'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'callbacks/omniauth' }
+  devise_for :users,
+             controllers: {
+               omniauth_callbacks: 'callbacks/omniauth', sessions: 'sessions'
+             }
 
   resources :users
 end
