43cf31957ae87c3de053c0f136703c8448bea220
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-14 21:12:28 -0700

    add oauth columns to users

---
+++ db/migrate/20200715040929_add_omniauth_to_users.rb
@@ -0,0 +1,8 @@
+class AddOmniauthToUsers < ActiveRecord::Migration[6.0]
+  def change
+    change_table :users, bulk: true do |t|
+      t.string :provider
+      t.string :uid
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_715_034_110) do
+ActiveRecord::Schema.define(version: 20_200_715_040_929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20_200_715_034_110) do
     t.datetime 'locked_at'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.string 'provider'
+    t.string 'uid'
     t.index %w[email], name: 'index_users_on_email', unique: true
     t.index %w[unlock_token], name: 'index_users_on_unlock_token', unique: true
   end
