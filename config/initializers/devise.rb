baf6ebed01109e597797751a078786d76f6ca1aa
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-15 01:27:41 -0700

    protect test env

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -270,10 +270,12 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github,
-                  Rails.application.credentials.github[:client_id],
-                  Rails.application.credentials.github[:client_secret],
-                  scope: 'user,repo:invite'
+  unless Rails.env.test?
+    config.omniauth :github,
+                    Rails.application.credentials.github[:client_id],
+                    Rails.application.credentials.github[:client_secret],
+                    scope: 'user,repo:invite'
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
