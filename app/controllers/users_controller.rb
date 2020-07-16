7d9c9a89e18488bc5c4acc3b0ad161f367efa7f6
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-15 22:15:29 -0700

    fix user controller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
+    if current_user.id.to_s == params[:id]
+      @user = current_user
+    else
+      flash.alert = "Permission denied"
+      redirect_to current_user
+    end
   end
 end
