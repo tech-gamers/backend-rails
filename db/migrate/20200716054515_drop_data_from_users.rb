8b2a32caea632f6d4436d1b690aff4595866739e
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-15 22:45:51 -0700

    drop data json

---
+++ db/migrate/20200716054515_drop_data_from_users.rb
@@ -0,0 +1,5 @@
+class DropDataFromUsers < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :data
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_053152) do
+ActiveRecord::Schema.define(version: 2020_07_16_054515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2020_07_16_053152) do
     t.string "provider"
     t.string "uid"
     t.string "encrypted_password"
-    t.jsonb "data"
     t.string "username"
     t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
