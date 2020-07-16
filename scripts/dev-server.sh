88d3efe3a0053e64d9ae1dd8c600136c2a8310d3
Yang Liu <jeremyrobturtle@gmail.com>
2020-07-16 02:13:23 -0700

    [DEV] fix conditional server

--- scripts/dev-server.sh
+++ scripts/dev-server.sh
@@ -4,11 +4,11 @@ if [ -f tmp/pids/server.pid ]; then
   rm -f tmp/pids/server.pid
 fi
 
-if [ ${RAILS_ENV} == 'development' && -n ${DEBUG} ]; then
+if [ -z ${DEBUG} ]; then
+  bin/rails s -p ${PORT} -b 0.0.0.0
+else
   bin/rdebug-ide \
     --host 0.0.0.0 \
     --port ${DEBUG_PORT} \
     -- bin/rails s -p ${PORT} -b 0.0.0.0
-else
-  bin/rails s -p ${PORT} -b 0.0.0.0
 fi
